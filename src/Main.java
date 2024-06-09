b468a89cf05e456fbb8b679d8aa99b4a75870a24
Your name <Your email>
2024-06-09 20:58:42 +0300

    Added basic window

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        JFrame window = null;
+        JFrame window = new JFrame();
         System.out.println("Start");
         Start start = Start.startGame(window);
         System.out.println("Finish");
